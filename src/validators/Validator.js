e9ace977a1c36c927788c58f39dbda0aeb173ab4
bbuchanan9 <bennett.buchanan@scality.com>
2019-04-30 16:37:16 -0700

    bugfix: S3C-2155 Time range validation
    * Check that both start and end times are in the
      past
    * Do not allow the start time to be greater than
      the end time

--- src/validators/Validator.js
+++ src/validators/Validator.js
@@ -26,10 +26,11 @@ const keyError = new Map([
     ['users', errors.InvalidParameterValue],
     ['service', errors.InvalidParameterValue],
     ['timeRange', errors.InvalidParameterValue.customizeDescription(
-        'Timestamps must be one of the following intervals for any day/hour' +
-            ' (mm:ss:SS) - start must be one of [00:00:000, 15:00:000, ' +
-            '30:00:000, 45:00:000], end must be one of [14:59:999,' +
-            ' 29:59:999, 44:59:999, 59:59:999].'
+        'Timestamps must be one of the following intervals for any past ' +
+        'day/hour (mm:ss:SS) - start must be one of [00:00:000, 15:00:000, ' +
+        '30:00:000, 45:00:000], end must be one of [14:59:999, ' +
+        '29:59:999, 44:59:999, 59:59:999]. Start must not be greater than ' +
+        'end.'
     )],
 ]);
 
