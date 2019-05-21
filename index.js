d94eae8ef564e9fcba8d6e6d869e9de088336e54
bbuchanan9 <bennett.buchanan@scality.com>
2019-05-21 14:55:00 -0700

    bugfix: S3C-2076 Add UtapiReindex

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ module.exports = {
     UtapiServer: require('./lib/server.js'),
     UtapiClient: require('./lib/UtapiClient.js'),
     UtapiReplay: require('./lib/UtapiReplay.js'),
+    UtapiReindex: require('./lib/UtapiReindex.js'),
 };
