750188f39b86250de15df7fbe0b838e2caed137d
Jianqin Wang <jianqinwang94@gmail.com>
2019-06-05 17:37:27 -0700

    bugfix: S3C-2019 redis sentinel authentication

--- lib/reindex/s3_bucketd.py
+++ lib/reindex/s3_bucketd.py
@@ -10,12 +10,13 @@ import sys
 from threading import Thread
 from concurrent.futures import ThreadPoolExecutor
 
-if len(sys.argv) == 6:
+if len(sys.argv) == 7:
     ip = sys.argv[1]
     port = sys.argv[2]
     sentinel_cluster_name = sys.argv[3]
-    bucketd_host = sys.argv[4]
-    bucketd_port = sys.argv[5]
+    sentinel_password = sys.argv[4]
+    bucketd_host = sys.argv[5]
+    bucketd_port = sys.argv[6]
     print("Sentinel IP used: %s" % ip)
     print("Sentinel port used: %s" % port)
     print("Sentinel cluster name used: %s" % sentinel_cluster_name)
@@ -25,6 +26,7 @@ else:
     ip = "127.0.0.1"
     port = "16379"
     sentinel_cluster_name = "scality-s3"
+    sentinel_password = ''
     bucketd_host =  "127.0.0.1"
     bucketd_port = "9000"
 
@@ -37,7 +39,7 @@ class updateRedis():
 
     def __init__(self, ip="127.0.0.1", port="16379", sentinel_cluster_name="scality-s3"):
 
-        r = redis.Redis(host=ip, port=port, db=0)
+        r = redis.Redis(host=ip, port=port, db=0, password=sentinel_password)
         self._ip, self._port = r.sentinel_get_master_addr_by_name(sentinel_cluster_name)
 
     def read(self, resource, name):
