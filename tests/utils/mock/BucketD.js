4d6fd396932b6c11f8d8b8e6be6a5846581c6b50
bbuchanan9 <bennett.buchanan@scality.com>
2019-07-25 14:34:15 -0700

    bugfix: S3C-2342 BucketD listing functional tests

--- tests/utils/mock/BucketD.js
+++ tests/utils/mock/BucketD.js
@@ -12,27 +12,68 @@ const app = express();
 class BucketD {
     constructor() {
         this._server = null;
+        this._bucketCount = 0;
+        this._bucketContent = {};
+        this._buckets = [];
     }
 
-    _getUsersBucketResponse() {
-        const key = `${CANONICAL_ID}${constants.splitter}${BUCKET_NAME}`;
-        const value = JSON.stringify({ creationDate: new Date() });
+    clearBuckets() {
+        this._bucketContent = {};
+        this._buckets = [];
+        this._bucketCount = 0;
+        return this;
+    }
+
+    setBucketCount(count) {
+        this._bucketCount = count;
+        return this;
+    }
+
+    setBucketContent({ bucketName, contentLength }) {
+        const metadata = new ObjectMD()
+            .setContentLength(contentLength)
+            .getValue();
+        this._bucketContent[bucketName] = [{
+            key: OBJECT_KEY,
+            value: JSON.stringify(metadata),
+        }];
+        return this;
+    }
+
+    createBuckets() {
+        const buckets = [];
+        for (let i = 0; i < this._bucketCount; i++) {
+            const { splitter } = constants;
+            const entry = {
+                key: `${CANONICAL_ID}${splitter}${BUCKET_NAME}-${i + 1}`,
+                value: JSON.stringify({ creationDate: new Date() }),
+            };
+            buckets.push(entry);
+        }
+        this._buckets = buckets;
+        return this;
+    }
+
+    _getUsersBucketResponse(req) {
         const body = {
             CommonPrefixes: [],
-            IsTruncated: false,
-            Contents: [{ key, value }],
         };
+        const maxKeys = parseInt(req.query.maxKeys, 10);
+        if (req.query.marker) {
+            body.IsTruncated = false;
+            body.Contents = this._buckets.slice(maxKeys);
+        } else {
+            body.IsTruncated = maxKeys < this._bucketCount;
+            body.Contents = this._buckets.slice(0, maxKeys);
+        }
         return JSON.stringify(body);
     }
 
-    _getBucketResponse() {
-        const key = OBJECT_KEY;
-        const metadata = new ObjectMD().setContentLength(1024).getValue();
-        const value = JSON.stringify(metadata);
+    _getBucketResponse(bucketName) {
         const body = {
             CommonPrefixes: [],
             IsTruncated: false,
-            Versions: [{ key, value }],
+            Versions: this._bucketContent[bucketName] || [],
         };
         return JSON.stringify(body);
     }
@@ -40,9 +81,11 @@ class BucketD {
     _initiateRoutes() {
         app.param('bucketName', (req, res, next, bucketName) => {
             if (bucketName === constants.usersBucket) {
-                req.body = this._getUsersBucketResponse();
+                // eslint-disable-next-line no-param-reassign
+                req.body = this._getUsersBucketResponse(req);
             } else {
-                req.body = this._getBucketResponse();
+                // eslint-disable-next-line no-param-reassign
+                req.body = this._getBucketResponse(bucketName);
             }
             next();
         });
