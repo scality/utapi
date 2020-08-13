c97f210b0ecdf690a86a80e6efdf7dd51a2465a1
Taylor McKinnon <mail@tmacs.space>
2020-08-13 12:53:14 -0700

    bf(S3C-3255): Ensure ingested events timestamp precision

--- libV2/server/API/metrics/ingestMetric.js
+++ libV2/server/API/metrics/ingestMetric.js
@@ -1,11 +1,15 @@
 const errors = require('../../../errors');
 const { UtapiMetric } = require('../../../models');
 const { client: cacheClient } = require('../../../cache');
+const { convertTimestamp } = require('../../../utils');
 
 async function ingestMetric(ctx, params) {
     let metrics;
     try {
-        metrics = params.body.map(m => new UtapiMetric(m));
+        metrics = params.body.map(m => new UtapiMetric({
+            ...m,
+            timestamp: convertTimestamp(m.timestamp),
+        }));
     } catch (error) {
         throw errors.InvalidRequest;
     }
