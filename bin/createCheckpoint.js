99493dea8bd052403ef0a1a553b011532d120c6d
Taylor McKinnon <mail@tmacs.space>
2021-01-25 21:24:21 -0800

    convert entrypoints

--- bin/createCheckpoint.js
+++ bin/createCheckpoint.js
@@ -1,12 +1,13 @@
 const { tasks } = require('..');
 const { LoggerContext } = require('../libV2/utils');
+const { clients: warp10Clients } = require('../libV2/warp10');
 
 const logger = new LoggerContext({
     task: 'CreateCheckpoint',
 });
 
 
-const task = new tasks.CreateCheckpoint();
+const task = new tasks.CreateCheckpoint({ warp10: [warp10Clients[0]] });
 
 task.setup()
     .then(() => logger.info('Starting checkpoint creation'))
--- bin/repair.js
+++ bin/repair.js
@@ -1,12 +1,13 @@
 const { tasks } = require('..');
 const { LoggerContext } = require('../libV2/utils');
+const { clients: warp10Clients } = require('../libV2/warp10');
 
 const logger = new LoggerContext({
     task: 'Repair',
 });
 
 
-const task = new tasks.RepairTask();
+const task = new tasks.RepairTask({ warp10: [warp10Clients[0]] });
 
 task.setup()
     .then(() => logger.info('Starting Repair daemon'))
