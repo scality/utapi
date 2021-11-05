2d60f5c480d26f9d0323ddeb515e3187fb694b59
Taylor McKinnon <mail@tmacs.space>
2021-11-05 11:50:58 -0700

    stash

--- eve/workers/unit_and_feature_tests/run_ft_tests.bash
+++ eve/workers/unit_and_feature_tests/run_ft_tests.bash
@@ -17,6 +17,6 @@ if [ -z "$SETUP_CMD" ]; then
   SETUP_CMD="start"
 fi
 
-UTAPI_INTERVAL_TEST_MODE=$1 npm $SETUP_CMD 2>&1 | tee -a "/artifacts/setup_$2.log" &
+UTAPI_FILTER_BUCKET_DENY='deny-this-bucket' UTAPI_INTERVAL_TEST_MODE=$1 npm $SETUP_CMD 2>&1 | tee -a "/artifacts/setup_$2.log" &
 bash tests/utils/wait_for_local_port.bash $PORT 40
 UTAPI_INTERVAL_TEST_MODE=$1 npm run $2 | tee -a "/artifacts/test_$2.log"
