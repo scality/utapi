f44b63a3fd179bcd00649704cb3cedc1c9c90950
Taylor McKinnon <mail@tmacs.space>
2021-12-02 16:21:50 -0800

    impr(UTAPI-55): Improve warp 10 request error logging

--- libV2/server/API/metrics/listMetrics.js
+++ libV2/server/API/metrics/listMetrics.js
@@ -2,6 +2,7 @@ const errors = require('../../../errors');
 const { serviceToWarp10Label, operationToResponse } = require('../../../constants');
 const { convertTimestamp, iterIfError } = require('../../../utils');
 const { clients: warp10Clients } = require('../../../warp10');
+const { warp10 } = require('../../../config');
 
 const emptyOperationsResponse = Object.values(operationToResponse)
     .reduce((prev, key) => {
@@ -28,47 +29,63 @@ async function listMetric(ctx, params) {
         end = Date.now();
     }
 
-    // A separate request will be made to warp 10 per requested resource
-    const results = await Promise.all(
-        resources.map(async ({ resource, id }) => {
-            const labels = { [labelName]: id };
+    let results;
 
-            const res = await iterIfError(warp10Clients, warp10 => {
-                const options = {
-                    params: {
-                        start: convertTimestamp(start).toString(),
-                        end: convertTimestamp(end).toString(),
+    try {
+        // A separate request will be made to warp 10 per requested resource
+        results = await Promise.all(
+            resources.map(async ({ resource, id }) => {
+                const labels = { [labelName]: id };
+
+                const res = await iterIfError(warp10Clients, warp10 => {
+                    const options = {
+                        params: {
+                            start: convertTimestamp(start).toString(),
+                            end: convertTimestamp(end).toString(),
+                            labels,
+                            node: warp10.nodeId,
+                        },
+                        macro: 'utapi/getMetrics',
+                    };
+                    return warp10.exec(options);
+                }, error => ctx.logger.error('error during warp 10 request', {
+                    error,
+                    requestParams: {
+                        start,
+                        end,
                         labels,
-                        node: warp10.nodeId,
+                        nodeId: warp10.nodeId,
                     },
-                    macro: 'utapi/getMetrics',
+                }));
+
+                if (res.result.length === 0) {
+                    ctx.logger.error('unable to retrieve metrics', { resource, type: params.level });
+                    throw errors.InternalError;
+                }
+
+                const rawMetrics = JSON.parse(res.result[0]);
+
+                // Due to various error cases it is possible for metrics in utapi to go negative.
+                // As this is nonsensical to the user we replace any negative values with zero.
+                const metrics = {
+                    storageUtilized: rawMetrics.storageUtilized.map(positiveOrZero),
+                    numberOfObjects: rawMetrics.numberOfObjects.map(positiveOrZero),
+                    incomingBytes: positiveOrZero(rawMetrics.incomingBytes),
+                    outgoingBytes: positiveOrZero(rawMetrics.outgoingBytes),
+                    operations: rawMetrics.operations,
                 };
-                return warp10.exec(options);
-            });
 
-            if (res.result.length === 0) {
-                ctx.logger.error('unable to retrieve metrics', { resource, type: params.level });
-                throw errors.InternalError;
-            }
+                return {
+                    resource,
+                    metrics,
+                };
+            }),
+        );
+    } catch (error) {
+        ctx.logger.error('error fetching metrics from warp10', { error });
+        throw errors.InternalError;
+    }
 
-            const rawMetrics = JSON.parse(res.result[0]);
-
-            // Due to various error cases it is possible for metrics in utapi to go negative.
-            // As this is nonsensical to the user we replace any negative values with zero.
-            const metrics = {
-                storageUtilized: rawMetrics.storageUtilized.map(positiveOrZero),
-                numberOfObjects: rawMetrics.numberOfObjects.map(positiveOrZero),
-                incomingBytes: positiveOrZero(rawMetrics.incomingBytes),
-                outgoingBytes: positiveOrZero(rawMetrics.outgoingBytes),
-                operations: rawMetrics.operations,
-            };
-
-            return {
-                resource,
-                metrics,
-            };
-        }),
-    );
 
     // Convert the results from warp10 into the expected response format
     const resp = results
